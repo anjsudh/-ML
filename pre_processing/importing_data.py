4a7e0efa6f5c0a28ba571b371a8c9f0698daaed2
anjsudh <anjsudh24@gmail.com>
2018-04-03 18:52:20 +0530

    [Data preprocessing] Add POC for importing dataset, removing missing data and handling categorical data

---
+++ pre_processing/importing_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+#Importing the data set
+dataset = pd.read_csv('./pre_processing/data/pre_processing_data.csv');
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
