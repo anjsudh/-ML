d1adc4c0d594e90d09b23503256559e4c83bfca2
anjsudh <anjsudh24@gmail.com>
2018-04-03 19:49:09 +0530

    [Data preprocessing] Add POC for feature scaling

--- pre_processing/feature_scaling.py
+++ pre_processing/feature_scaling.py
@@ -30,3 +30,9 @@ Y = labelEncoder_Y.fit_transform(Y)
 #Splitting the dataset into Training and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+#Feature scaling  - putting variables in same range
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
